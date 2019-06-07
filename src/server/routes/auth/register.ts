c3f64ce620d398b4466e5107de4498caf4c7edf3
Hayden <chfrank9011@gmail.com>
2019-06-07 12:28:59 -0500

    fixing array positioning on register function

--- src/server/routes/auth/register.ts
+++ src/server/routes/auth/register.ts
@@ -11,9 +11,10 @@ router.post('/', async (req, res, next) => {
         let author = req.body;
         author.password = HashPassword(req.body.password);
         let data = Object.values(author); 
-        let result: any = await DB.Authors.insertAuthor(data);
+        let result: any = await DB.Authors.insertAuthor([author.email, author.name, author.password]);
         let token = await CreateToken({ authorid: result.insertId });
         console.log(result);
+        console.log(data);
         res.json({
             token,
             role: 'admin',
